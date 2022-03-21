11dbc6a3b89178c9a5b3a50c9d8851450abe4b45
kavyajayan <kavyajayachandran@Kavyas-MacBook-Pro.local>
2022-03-21 15:32:30 +0530

    Initialization and workflow creation

---
+++ src/main/java/com/example/githubactionsspike/GithubActionsSpikeApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsspike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsSpikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsSpikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubactionsspike/GithubActionsSpikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsspike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsSpikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
